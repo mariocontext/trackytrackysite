a3a3258aa6ac7809381dbf73a477a203bd5b3048
Mario Noble <mrmarionoble@gmail.com>
2019-10-26 15:31:18 -0700

    style changes to nav

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
+  <div class="container mx-auto px-4 max-w-6xl">
+    <header class="flex flex-row max-w-xl space-around py-2">
+      <strong class="inline-block">
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
@@ -10,7 +10,12 @@
         <g-link class="nav__link" to="/faq/">FAQ</g-link>
       </nav>
     </header>
-    <slot />
+    <div role="main">
+      <slot />
+    </div>
+    <footer role="banner">
+      <p>My copyright info goes here</p>
+    </footer>
   </div>
 </template>
 
@@ -23,34 +28,4 @@ query {
 </static-query>
 
 <style>
-.myspecial-style {
-  color: #f90;
-  @apply text-2xl;
-}
-body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
 </style>
