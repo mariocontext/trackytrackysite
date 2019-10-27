1c943d854a8fbdf133305596af86e38916156b6d
Mario Noble <mrmarionoble@gmail.com>
2019-10-27 13:37:35 -0700

    added more sketchy styles to use in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,36 @@ module.exports = {
           borderBottomRightRadius: "225px 15px",
           borderTopLeftRadius: "255px 15px",
           borderTopRightRadius: "15px 225px"
+        },
+        ".sketchy-2": {
+          borderBottomLeftRadius: "185px 25px",
+          borderBottomRightRadius: "20px 205px",
+          borderTopLeftRadius: "125px 25px",
+          borderTopRightRadius: "10px 205px"
+        },
+        ".sketchy-3": {
+          borderBottomLeftRadius: "225px 15px",
+          borderBottomRightRadius: "15px 225px",
+          borderTopLeftRadius: "15px 225px",
+          borderTopRightRadius: "1255px 15px"
+        },
+        ".sketchy-4": {
+          borderBottomLeftRadius: "125px 115px",
+          borderBottomRightRadius: "155px 25px",
+          borderTopLeftRadius: "15px 225px",
+          borderTopRightRadius: "25px 150px"
+        },
+        ".sketchy-5": {
+          borderBottomLeftRadius: "20px 115px",
+          borderBottomRightRadius: "15px 105px",
+          borderTopLeftRadius: "250px 15px",
+          borderTopRightRadius: "125px 85px"
+        },
+        ".sketchy-6": {
+          borderBottomLeftRadius: "15px 205px",
+          borderBottomRightRadius: "20px 15px",
+          borderTopLeftRadius: "28px 125px",
+          borderTopRightRadius: "100px 30px"
         }
       };
       addUtilities(newUtilities, ["responsive", "hover"]);
